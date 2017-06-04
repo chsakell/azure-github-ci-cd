974f4137d4f69f97b859d5f0542621d1c9f69614
chsakell <chsakell@gmail.com>
2017-06-04 19:53:50 +0300

    feat(coverage): add coverage features

---
+++ coverage.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+# Install OpenCover and ReportGenerator, and save the path to their executables.
+nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
+nuget install -Verbosity quiet -OutputDirectory packages -Version 2.4.5.0 ReportGenerator
+
+OPENCOVER=$PWD/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
+REPORTGENERATOR=$PWD/packages/ReportGenerator.2.4.5.0/tools/ReportGenerator.exe
+
+CONFIG=Release
+# Arguments to use for the build
+DOTNET_BUILD_ARGS="-c $CONFIG"
+# Arguments to use for the test
+DOTNET_TEST_ARGS="$DOTNET_BUILD_ARGS"
+
+echo CLI args: $DOTNET_BUILD_ARGS
+
+echo Restoring
+
+dotnet restore
+
+echo Building
+
+dotnet build $DOTNET_BUILD_ARGS
+
+echo Testing
+
+coverage=./coverage
+rm -rf $coverage
+mkdir $coverage
+
+dotnet test -f netcoreapp1.1 $DOTNET_TEST_ARGS azure-github-ci-cd.Tests/azure-github-ci-cd.Tests.csproj
+
+echo "Calculating coverage with OpenCover"
+$OPENCOVER \
+  -target:"c:\Program Files\dotnet\dotnet.exe" \
+  -targetargs:"test -f netcoreapp1.1 $DOTNET_TEST_ARGS azure-github-ci-cd.Tests/azure-github-ci-cd.Tests.csproj" \
+  -mergeoutput \
+  -hideskipped:File \
+  -output:$coverage/coverage.xml \
+  -oldStyle \
+  -filter:"+[azure-github-ci-cd*]* -[azure-github-ci-cd.*Tests*]*" \
+  -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.1 \
+  -register:user
+
+echo "Generating HTML report"
+$REPORTGENERATOR \
+  -reports:$coverage/coverage.xml \
+  -targetdir:$coverage \
+  -verbosity:Error
