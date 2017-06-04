0111d852893e5c1e4e177f53d3cec217e4784c6f
chsakell <chsakell@gmail.com>
2017-06-04 11:04:34 +0300

    add xUnit Tests project

---
+++ azure-github-ci-cd.Tests/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+using azure_github_ci_cd.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace azure_github_ci_cd.Tests
+{
+    public class HomeControllerTest
+    {
+
+        public HomeControllerTest()
+        {
+        }
+
+        [Fact(DisplayName = "Index should return default view")]
+        public void Index_should_return_default_view()
+        {
+            var controller = new HomeController();
+            var viewResult = (ViewResult)controller.Index();
+            var viewName = viewResult.ViewName;
+
+            Assert.True(string.IsNullOrEmpty(viewName) || viewName == "Index");
+        }
+
+    }
+}
